2d30917aa7a592d4002cceb344fcdee47ae152bb
dustyy47 <dustydev47@yandex.ru>
2023-08-31 23:22:51 +0300

    types for user content

--- src/App.tsx
+++ src/App.tsx
@@ -38,6 +38,8 @@ const mockUsersCards: IUserCard[] = [
 ];
 
 export function App() {
+
+
   return (
     <>
       <Header />
--- src/types/index.ts
+++ src/types/index.ts
@@ -11,4 +11,50 @@ export interface IUserCard {
   status: IUserStatus;
 }
 
+export interface IFullUserInfo {
+  user: IUser,
+  notes: INote[],
+  consulations: IConsulation[]
+  videos: IVideo[]
+  events: IEvent[]
+}
+
+export interface INote {
+  date: string
+  content: string
+}
+
+export type ConsultationType = "online" | "offline"
+export type IConsulationStatus = ""
+
+export interface IConsulation{
+  id: string
+  type: ConsultationType 
+  date: string
+  time: {
+    start: string
+    end: string
+  }
+}
+
+export interface IVideo{
+  id: string
+  preview: string,
+  title: string
+  author: string
+  dates: {
+    start: string
+    end: string
+  }
+}
+
+
+export type EventType = "web"
+export interface IEvent{
+  id: string
+  type: EventType
+  date: string
+  time: string
+}
+
 export type IUserStatus = "newNote" | "alert";
